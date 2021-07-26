34145137cc84e814fee5fe5ff6cb6ccd9ae5b827
Marcos Soares <38232965+programmerMarcos@users.noreply.github.com>
2021-07-26 06:33:55 -0300

    Update calculoPiThreads.py
    programa terminado

--- calculoPiThreads.py
+++ calculoPiThreads.py
@@ -1,25 +1,59 @@
+import threading
+import os
+import timeit
+import statistics
+from math import sqrt
 
-import time
-resultsPI = []
-temposExecucao = []
-i = 0
-tam = 10000
+NUM_EXECUCOES = int(input('Digite quantas execuções:'))
+qtThreads = int(input('Digite quantas Threads:'))
 pi = 0
-def calcPI():
+tam = 1000000000
+exePorThread = int(tam/qtThreads)
+nInicial = 0
 
-    global pi
-    pi = 0
-    for j in range(tam):
-        pi += pow(-1,j) / (2*j+1)
+threads = []
+temposExe = []
+termo = 0.0
 
-    resultsPI.append(pi*4)
+mediaTempoExecucao = 0.0
+varTempoExecucao = 0.0
+class Minhathread(threading.Thread):
+    def __init__(self,indice, mutex):
+        self.indice = indice
+        self.mutex = mutex
+        threading.Thread.__init__(self)
+    def run(self):
+        global pi
+        for i in range(self.indice,tam):
+            with self.mutex:
+                pi += pow(-1, i) / (2*i+1)
+
+stdoutmutex = threading.Lock()        
+
+for exe in range(NUM_EXECUCOES):
+    inicio = timeit.default_timer()
+    for i in range(qtThreads):
+
+        thread = Minhathread(nInicial, stdoutmutex)
+        thread.start()
+        #print(f"calc da T[{i}]{pi}")
+        threads.append(thread)
+        nInicial+=exePorThread
+    fim = timeit.default_timer() - inicio
+    temposExe.append(fim)
+
+for x in range(NUM_EXECUCOES):
+    mediaTempoExecucao += temposExe[x]/NUM_EXECUCOES
 
 
-for i in range(10):
-    inicio = time.time()
-    calcPI()
-    fim = time.time()
-    temposExecucao.append(fim-inicio)
-for i in range(len(resultsPI)):
-    print(resultsPI[i])
-    print(f'tempo->{temposExecucao[i]*1000:.2f}ms')
+for x in range(NUM_EXECUCOES):
+    termo = temposExe[x] - mediaTempoExecucao
+    varTempoExecucao += pow(termo,2)/NUM_EXECUCOES
+
+
+dp = sqrt(varTempoExecucao)
+coeficienteVariacao = (dp/mediaTempoExecucao)
+print("Coeficiente de variacao:{:.5f}%".format(coeficienteVariacao))
+print("Media tempo de Execucao:{:.5f}s".format(mediaTempoExecucao))
+print("Desvio padrao:{:.5f}".format(dp))
+print("VALOR DE PI:{:.7f}".format(pi*4))
